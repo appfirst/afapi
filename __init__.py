3d875dc5708e6384d06e0619ce8bc544f7c60ffe
Mike Okner <michael@appfirst.com>
2014-04-16 20:57:01 -0500

    Finished implemeting server API methods

--- __init__.py
+++ __init__.py
@@ -16,6 +16,8 @@ __email__ = "michael@appfirst.com"
 __status__ = "Development"
 
 
+import time
+import datetime
 try:
     import simplejson as json
 except ImportError:
@@ -98,15 +100,12 @@ class AppFirstApi(object):
 
     def update_server(self, host_id, data):
         """
-        Edit a server. Data argument should look like 'nickname=new_nick'
+        Edit a server. Data argument should be a dict like {'nickname': 'new_nick'}
 
         http://support.appfirst.com/apis/servers/#serverid
         """
-        # Sanity checks
-        if not isinstance(data, str):
-            raise TypeError("Data must be a string")
-        if '=' not in data:
-            raise ValueError("Invalid data string format")
+        if not isinstance(data, dict):
+            raise TypeError("Data must be a dictionary")
 
         return self._make_api_request('/servers/{0}/'.format(host_id), data=data)
 
@@ -120,22 +119,72 @@ class AppFirstApi(object):
         return self._make_api_request('/servers/{0}/'.format(host_id), method='DELETE')
 
 
-    def get_server_data(self, host_id, limit=5):
+    def get_server_data(self, host_id, **kwargs):
         """
         Retrieves data for the given server.
 
+        Arguments:
+            - host_id: required, first arg, non-keyword.
+            - num: optional keyword, max number of data points to retrieve. Defaults to one.
+            - end: optional keyword, retrieves data from this datetime object backwards. Defaults to current time.
+            - start: optional keyword, won't retrieve data from before this datetime object.
+            - time_step: optional keyword, time step for the data. Can be 'Minute', 'Hour', 'Day'. Default is 'Minute'.
+
         http://support.appfirst.com/apis/servers/#serveriddata
         """
-        return NotImplemented
+        params = {'num': kwargs.get('num', 1)}
+        end = kwargs.get('end', None)
+        start = kwargs.get('start', None)
+        time_step = kwargs.get('time_step', 'Minute')
+
+        # Sanity Checks
+        if end is not None and not isinstance(end, datetime.datetime):
+            raise TypeError("end value must be a datetime.datetime instance")
+        else:
+            params['end'] = time.mktime(end.timetuple())
+            
+        if start is not None and not isinstance(start, datetime.datetime):
+            raise TypeError("start value must be a datetime.datetime instance")
+        else:
+            params['start'] = time.mktime(start.timetuple())
+            
+        if time_step not in ['Minute', 'Hour', 'Day']:
+            raise ValueError("Invalid time_step: {0}".format(time_step))
+        else:
+            params['time_step'] = time_step
+
+        # Send request
+        return self._make_api_request('/servers/{0}/data/'.format(host_id), params=params)
 
 
-    def get_server_outages(self, host_id):
+    def get_server_outages(self, host_id, **kwargs):
         """
         This API lists the recent outages for the given server.
 
+        Arguments:
+            - host_id: required, first arg, non-keyword.
+            - num: optional keyword, max number of data points to retrieve. Defaults to one.
+            - end: optional keyword, retrieves data from this datetime object backwards. Defaults to current time.
+            - start: optional keyword, won't retrieve data from before this datetime object.
+
         http://support.appfirst.com/apis/servers/#serveridoutages
         """
-        return NotImplemented
+        params = {'limit': kwargs.get('num', 1)}
+        end = kwargs.get('end', None)
+        start = kwargs.get('start', None)
+
+        # Sanity Checks
+        if end is not None and not isinstance(end, datetime.datetime):
+            raise TypeError("end value must be a datetime.datetime instance")
+        else:
+            params['end'] = time.mktime(end.timetuple())
+            
+        if start is not None and not isinstance(start, datetime.datetime):
+            raise TypeError("start value must be a datetime.datetime instance")
+        else:
+            params['start'] = time.mktime(start.timetuple())
+
+        return self._make_api_request('/servers/{0}/outages/'.format(host_id))
 
 
     def get_polled_data_config(self, host_id):
@@ -144,16 +193,23 @@ class AppFirstApi(object):
 
         http://support.appfirst.com/apis/servers/#serveridpolleddata
         """
-        return NotImplemented
+        return self._make_api_request('/servers/{0}/polled_data_config/'.format(host_id))
 
 
-    def update_polled_data_config(self, host_id):
+    def update_polled_data_config(self, host_id, data):
         """
         Update the Polled Data config on a server.
 
+        Data should be a dictionary of updated fields like
+        {'file_contents': 'Sample Content',
+         'file_path': '/usr/local/nagios/etc/nrpe.cfg'}
+
         http://support.appfirst.com/apis/servers/#serveridpolleddata
         """
-        return NotImplemented
+        if not isinstance(data, dict):
+            raise TypeError("Data must be a dictionary")
+
+        return self._make_api_request('/servers/{0}/polled_data_config/'.format(host_id), method='PUT', data=data)
 
 
     def get_server_tags(self, host_id):
@@ -162,16 +218,20 @@ class AppFirstApi(object):
 
         http://support.appfirst.com/apis/servers/#serveridtags
         """
-        return NotImplemented
+        return self._make_api_request('/servers/{0}/tags/'.format(host_id))
 
 
-    def update_server_tags(self, host_id):
+    def update_server_tags(self, host_id, new_tags):
         """
         Updates the server tags that this server belongs to.
 
         http://support.appfirst.com/apis/servers/#serveridtags
         """
-        return NotImplemented
+        if not isinstance(new_tags, list):
+            raise TypeError("new_tags must be a list")
+
+        params = {'server_tags': json.dumps(new_tags)}
+        return self._make_api_request('/servers/{0}/tags/'.format(host_id), params=params)
 
 
     def trigger_server_app_auto_detect(self, host_id):
@@ -181,24 +241,73 @@ class AppFirstApi(object):
 
         http://support.appfirst.com/apis/servers/#serveridautodetection
         """
-        return NotImplemented
+        return self._make_api_request('/servers/{0}/auto_detection/'.format(host_id))
 
 
     # Process APIs
-    def get_processes(self, host_id):
+    def get_processes(self, host_id, **kwargs):
         """
         Returns the list of processes on a server. See the Processes API
         documentation for more information about process objects.
 
+        Arguments:
+            - host_id: required, first argument, non-keyword
+            - end: optional keyword, datetime object for end-date of time range
+            - start: optional keyword, datetime object for start-date of time range
+
         http://support.appfirst.com/apis/servers/#serveridprocesses
         """
-        return NotImplemented
+        params = {}
+        end = kwargs.get('end', None)
+        start = kwargs.get('start', None)
+
+        # Sanity Checks
+        if end is not None and not isinstance(end, datetime.datetime):
+            raise TypeError("end value must be a datetime.datetime instance")
+        else:
+            params['end'] = time.mktime(end.timetuple())
+            
+        if start is not None and not isinstance(start, datetime.datetime):
+            raise TypeError("start value must be a datetime.datetime instance")
+        else:
+            params['start'] = time.mktime(start.timetuple())
+            
+        return self._make_api_request('/servers/{0}/processes/'.format(host_id), params=params)
 
 
-    def get_processes_data(self, host_id):
+    def get_processes_data(self, host_id, **kwargs):
         """
         Returns the list of a processes’ summary data on a particular server.
 
+        Arguments:
+            - host_id: required, first arg, non-keyword.
+            - num: optional keyword, max number of data points to retrieve. Defaults to one.
+            - end: optional keyword, retrieves data from this datetime object backwards. Defaults to current time.
+            - start: optional keyword, won't retrieve data from before this datetime object.
+            - time_step: optional keyword, time step for the data. Can be 'Minute', 'Hour', 'Day'. Default is 'Minute'.
+
         http://support.appfirst.com/apis/servers/#serveridprocessesdata
         """
-        return NotImplemented
+        params = {'num': kwargs.get('num', 1)}
+        end = kwargs.get('end', None)
+        start = kwargs.get('start', None)
+        time_step = kwargs.get('time_step', 'Minute')
+
+        # Sanity Checks
+        if end is not None and not isinstance(end, datetime.datetime):
+            raise TypeError("end value must be a datetime.datetime instance")
+        else:
+            params['end'] = time.mktime(end.timetuple())
+            
+        if start is not None and not isinstance(start, datetime.datetime):
+            raise TypeError("start value must be a datetime.datetime instance")
+        else:
+            params['start'] = time.mktime(start.timetuple())
+            
+        if time_step not in ['Minute', 'Hour', 'Day']:
+            raise ValueError("Invalid time_step: {0}".format(time_step))
+        else:
+            params['time_step'] = time_step
+
+        # Send request
+        return self._make_api_request('/servers/{0}/processes/data/'.format(host_id), params=params)
